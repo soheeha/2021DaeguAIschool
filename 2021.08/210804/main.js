2f3620e511dde6c8ef6faa64a042cc6f0b26ed3f
soheeha <86584306+soheeha@users.noreply.github.com>
2021-08-04 16:39:29 +0900

    210804 JS

---
+++ 2021.08/210804/main.js
@@ -0,0 +1,95 @@
+// comic 박스를 생성한 상태
+//변수 선언
+// var comic;
+
+
+// comic 박스 안에 원피스 데이터를 할당한 상태
+// 변수 초기화
+// comic = "원피스";
+
+
+//박스 안에 데이터가 잘 들어갔는지 확인하고 싶을 때
+// console.log(comic);
+
+//변수 선언과 초기화를 동시에 하는 법
+// var apple = "사과";
+
+
+// 변수 동시 선언
+/*
+var a;
+var b;
+var c;
+*/
+
+// var a, b, c;
+
+
+// 변수 동시 초기화
+/*
+var d = 10;
+var e = 20;
+var f = 30;
+*/
+
+// var d = 10, e = 20, f = 30;
+
+
+
+//모든 변수는 선언과 동시에 undefined가 자동으로 할당된다.
+// var computer;
+// console.log(computer);
+
+
+/*
+var house;
+
+house = "집";
+console.log(house);
+
+house = "아파트";
+console.log(house);
+*/
+
+
+
+/*
+var house;
+console.log(house);
+
+// undefined -> 
+house = "집";
+console.log(house);
+*/
+
+
+//캐멀 케이스
+// var mathScoreStudent = 10;
+
+//스네이크 케이스
+// var math_score_student = 10;
+
+// 주의점
+// var math-score-student = 100;
+
+// var _math;
+// var $math;
+
+// var apple10;
+// var 10apple;
+
+// var w;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
