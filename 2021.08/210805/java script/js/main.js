caa9937ccea0c8db994972def7fa09a7e2803dd5
soheeha <86584306+soheeha@users.noreply.github.com>
2021-08-06 23:37:43 +0900

    210805 js

---
+++ 2021.08/210805/java script/js/main.js
@@ -0,0 +1,294 @@
+// comic 박스를 생성한 상태
+//변수 선언
+// var comic;
+
+
+// comic 박스 안에 원피스 데이터를 할당한 상태
+// 변수 초기화
+// comic = "원피스";
+
+
+//박스 안에 데이터가 잘 들어갔는지 확인하고 싶을 때
+// console.log(comic);
+
+//변수 선언과 초기화를 동시에 하는 법
+// var apple = "사과";
+
+
+// 변수 동시 선언
+/*
+var a;
+var b;
+var c;
+*/
+
+// var a, b, c;
+
+
+// 변수 동시 초기화
+/*
+var d = 10;
+var e = 20;
+var f = 30;
+*/
+
+// var d = 10, e = 20, f = 30;
+
+
+
+//모든 변수는 선언과 동시에 undefined가 자동으로 할당된다.
+// var computer;
+// console.log(computer);
+
+
+/*
+var house;
+
+house = "집";
+console.log(house);
+
+house = "아파트";
+console.log(house);
+*/
+
+
+
+/*
+var house;
+console.log(house);
+
+// undefined -> 
+house = "집";
+console.log(house);
+*/
+
+
+//캐멀 케이스
+// var mathScoreStudent = 10;
+
+//스네이크 케이스
+// var math_score_student = 10;
+
+// 주의점
+// var math-score-student = 100;
+
+// var _math;
+// var $math;
+
+// var apple10;
+// var 10apple;
+
+// var w;
+
+
+
+
+
+// 2021.08.05 데이터 타입
+
+
+// 문자열 (String)
+// var msg1 = "Hello World";
+// var msg2 = 'Welcome';
+
+// console.log(msg1);
+// console.log(msg2);
+
+
+// var msg3 = "Nice to meet you';";
+
+// She is girl
+// She's girl
+
+// var msg4 = "She's girl";
+// var msg5 = 'she\'s girl';
+
+// console.log(msg4);
+// console.log(msg5);
+
+
+
+
+// 숫자 (Number)
+
+// var num1 = 10;
+// var num2 = -10;
+// var num3 = 3.14;
+// var num4 = -3.14;
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+
+
+// console.log(num1 + num2);
+// console.log(num1 + num3);
+
+
+// 연산자
+// 숫자 사칙연산
+// var a = 20;
+// var b = 10;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+//문자열
+// var str1 = "20";
+// var str2 = "10";
+
+// console.log(str1 + str2);
+// console.log(str1 - str2);
+// console.log(str1 * str2);
+// console.log(str1 / str2);
+// console.log(str1 % str2);
+
+// var firstName = "Gildong";
+// var lastName = "Hong";
+
+// console.log(firstName + " " + lastName);
+
+
+
+// var str11 = "현재 시간은 ";
+// var time = 10;
+// var str12 = "시 입니다.";
+
+// console.log(str11 + time + str12);
+
+
+
+
+// ++, -- 연산자
+// var num10 = 10;
+
+// num10 = num10 - 1;
+// console.log(num10);
+// console.log(--num10); // 9
+// console.log(--num10); // 8
+
+// console.log(++num10);
+// console.log(++num10);
+
+
+// console.log(num10--);
+// console.log(num10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
